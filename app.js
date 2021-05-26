28a43501eab828e6b083597c3d0866333d358969
idasvensson12 <ida_svensson2001@hotmail.com>
2021-05-26 19:57:48 +0200

    1234

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const staticDir + __dirname + "\\static\\";
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(staticDir));
+
+app.get('/', (req, res) => res.sendFile(staticDir + "html\\index.html"))
+
+app.get('/contact', (req, res) => 
+res.sendFile(staticDir + "html\\contact.html"))
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
