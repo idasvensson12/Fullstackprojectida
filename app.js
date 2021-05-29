ca862fea6df52fd36bc2332d54ae90db0f9b733a
idasvensson12 <ida_svensson2001@hotmail.com>
2021-05-29 21:24:13 +0200

    12345

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const staticDir + __dirname + "\\static\\";
+const staticDir =__dirname + "\\static\\";
 
 app.use(express.static(staticDir));
-
+app.use(express.urlencoded());
+app.use(express.json());
 app.get('/', (req, res) => res.sendFile(staticDir + "html\\index.html"))
 
 app.get('/contact', (req, res) => 
-res.sendFile(staticDir + "html\\contact.html"))
+res.sendFile(staticDir + "html\\contact.html")
+);
+
+app.post('/contact', function (req, res) {
+    console.log(req.body.fname);
+    console.log(req.body.date);
+  res.redirect('/');
+})
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
